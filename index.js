b4c853db4aa9ec47afd1d29f7617ba6756b7426e
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-07-06 11:59:06 +0200

    database connection setup

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const dbConnection = require("./config/database");
 const app = express();
 
-app.listen(process.env.SERVER_PORT, () =>
-  console.log(`Server listening to port ${process.env.SERVER_PORT}...`)
-);
+const runServer = async () => {
+  await dbConnection();
+
+  app.listen(process.env.SERVER_PORT, () =>
+    console.log(`Server listening to port ${process.env.SERVER_PORT}...`)
+  );
+};
+
+runServer();
