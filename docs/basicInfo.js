4a1cb2e759f8b68ce9df8cccbeab0b8b6e407c8d
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-07-17 20:08:53 +0200

    Base docs

---
+++ docs/basicInfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  openapi: "3.0.3",
+  info: {
+    version: "1.0.0",
+    title: "Social Backend",
+    description:
+      "Practice of Mongoose implementation for a social network app.",
+  },
+};
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const dbConnection = require("./config/database");
 const { typeError } = require("./middleware/errors");
 
+const swaggerUI = require("swagger-ui-express");
+const docs = require("./docs/index");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -18,6 +21,8 @@ app.use(express.json());
 app.use("/users", require("./routes/users"));
 app.use("/posts", require("./routes/posts"));
 app.use("/comments", require("./routes/comments"));
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(docs));
 
 app.use(typeError);
 
