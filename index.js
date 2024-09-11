359bc698a670dc8c8608b06c4c8639f0d776f1ea
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-11 20:34:31 +0200

    added cors

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const runServer = async () => {
   app.listen(PORT, () => console.log(`Server listening to port ${PORT}...`));
 };
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use("/users", require("./routes/users"));
 app.use("/posts", require("./routes/posts"));
