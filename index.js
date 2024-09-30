0aca64a750cb6403b9748aa4ace382a82b701f9c
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-30 18:55:43 +0200

    changed origin in cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const runServer = async () => {
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
